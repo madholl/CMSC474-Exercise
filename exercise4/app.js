dbe1930e1f7552d51074ac16365a315374454d1d
madholl <madholl@udel.edu>
2021-12-17 15:06:10 -0500

    updated for exercise 5 submission

---
+++ exercise4/app.js
@@ -0,0 +1,58 @@
+const { create } = require("domain");
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+const app = express();
+const port = 8080;
+
+app.use( function ( req, res, next ) {
+const { url, path: routePath } = req ;
+console.log( 'Request: Timestamp:', new Date().toLocaleString(), ', URL (' + url + '), PATH (' + routePath + ').' ) ;
+next();
+});
+
+app.use('/', express.static(path.join(__dirname, '')))
+
+app.get('/api/v1/listUsers', function(req, res) {
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
+    console.log (data);
+    res.end(data);
+    });
+});
+
+app.delete('/api/v1/deleteUser', function(req, res){
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
+    data = JSON.parse(data);
+    delete data["user"+req.query["user"]];
+    fs.writeFile(__dirname + "/data/users.json", JSON.stringify(data), err => {
+    if (err) {
+    console.error(err);
+    return;
+    }
+    });
+    console.log(data);
+    res.end(JSON.stringify(data));
+    });
+    });
+
+app.post('/api/v1/addUser', function(req, res) {
+    fs.readFile(__dirname + "/data/" + "users.json", 'utf8', function(err, data){
+        data = JSON.parse(data);
+        let name = req.query["name"];
+        let password = req.query["password"];
+        let profession = req.query["profession"];
+        let  id = req.query["user"]; 
+        data['user' + id] = { name, password, profession, id };
+        fs.writeFile(__dirname + "/data/users.json", JSON.stringify(data), err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });        
+        res.end(JSON.stringify(data));
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}...`)
+});
